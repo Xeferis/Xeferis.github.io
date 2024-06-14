da036b48c2fda3fe3dbc8eee0df5bd7265b83aa1
Flo <93609174+Xeferis@users.noreply.github.com>
2024-06-14 10:29:41 +0200

    added cursor

---
+++ js/cursor.js
@@ -0,0 +1,30 @@
+var cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.styleopacity = 1;
+  cursor.style.left = x + 'px';
+  cursor.style.top = y + 'px';
+}
+);
+
+document.addEventListener('mousedown', function(e){
+    cursor.classList.add("click")
+  }
+);
+
+document.addEventListener('mouseup', function(e){
+    cursor.classList.remove("click")
+  }
+);
+
+document.addEventListener('mouseout', function(e){
+    cursor.style.opacity = 0;
+  }
+);
+
+document.addEventListener('mouseenter', function(e){
+    cursor.style.opacity = 1;
+  }
+);
