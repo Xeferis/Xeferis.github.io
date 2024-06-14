f6e95e86828233b646312d04deb2293e1be89156
Flo <93609174+Xeferis@users.noreply.github.com>
2024-06-14 10:47:16 +0200

    customized cursor

--- js/cursor.js
+++ js/cursor.js
@@ -1,6 +1,7 @@
 var cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', function(e){
+  cursor.classList.remove("hover")
   var x = e.clientX;
   var y = e.clientY;
   cursor.styleopacity = 1;
@@ -26,5 +27,10 @@ document.addEventListener('mouseout', function(e){
 
 document.addEventListener('mouseenter', function(e){
     cursor.style.opacity = 1;
+  }
+);
+
+document.addEventListener('mouseover', function(e){
+    cursor.classList.add("hover")
   }
 );
