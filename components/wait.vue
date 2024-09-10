bf4bc77fd524c4845c610ee1ccd095f1e67ce340
Florian <93609174+Xeferis@users.noreply.github.com>
2024-09-10 13:28:57 +0000

    Disabled Cursor effect on touch

--- components/wait.vue
+++ components/wait.vue
@@ -1,3 +1,3 @@
 <template>
-    <p class="group base hover:bg-second hover:shadow-glow-lg  hover:shadow-second cursor-auto hover:cursor-none hover:text-main w-fit font-mono tracking-widest text-3xl text-second transition-color duration-300"><A:mail></A:mail>nd now we wait <span class="blink border-r-[.10rem] border-second group-hover:border-main"></span></p>
+    <p class="group base hover:bg-second hover:shadow-glow-lg  hover:shadow-second cursor-auto hover:cursor-none hover:text-main w-fit font-mono tracking-widest text-3xl text-second transition-color duration-300">And now we wait <span class="blink border-r-[.10rem] border-second group-hover:border-main"></span></p>
 </template>
