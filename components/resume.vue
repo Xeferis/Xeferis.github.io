8a5463b962b4bc639e8255185ad1ed38d7520792
Flo <93609174+Xeferis@users.noreply.github.com>
2024-09-10 18:28:34 +0200

    Cursor in Background

---
+++ components/resume.vue
@@ -0,0 +1,13 @@
+<script setup>
+const data = lebenslauf();
+</script>
+
+<template>
+    <div>
+        <h1>Resume</h1>
+        <div id="data" v-for="d in data">
+            <h3>{{ d.title }}</h3>
+            <p>{{ d.absclhuss }}</p>
+        </div>
+    </div>
+</template>
