30284f613a8a6c8e5d4244b2c7121299c6112123
Florian <93609174+Xeferis@users.noreply.github.com>
2024-09-10 14:01:48 +0000

    Refined glow effect

--- public/js/customCursor.js
+++ public/js/customCursor.js
@@ -4,6 +4,34 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.appendChild(cursor);
   
     document.addEventListener('mousemove', (e) => {
+      const w = window.innerWidth;
+      const h = window.innerHeight;
+
+      const centerX = w / 2;
+      const centerY = h / 2;
+      const deltaX = e.clientX - centerX;
+      const deltaY = e.clientY - centerY;
+      const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+      const maxDistance = Math.sqrt(centerX * centerX + centerY * centerY);
+
+      const thresholdPercentage = 0.3; // 10% des maximalen Abstands als Schwellenwert
+
+      const thresholdDistance = maxDistance * thresholdPercentage;
+      let percentageDistance;
+
+      if (distance < thresholdDistance) {
+          percentageDistance = 1; // 100% innerhalb des Schwellenwerts
+      } else {
+          percentageDistance = 1 - (distance / maxDistance);
+      }
+
+
+      const dampingFactor = 1; // Je höher der Wert, desto schneller nimmt der Abstand ab
+      const dampedPercentageDistance = Math.pow(percentageDistance, dampingFactor);
+
+      cursor.style.opacity = dampedPercentageDistance;
+
+
       cursor.style.left = `${e.clientX}px`;
       cursor.style.top = `${e.clientY}px`;
     });
