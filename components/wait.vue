f76d64d97cbe7fbb9bc683333a3c54f2a8ea1bc0
Florian <93609174+Xeferis@users.noreply.github.com>
2024-09-19 09:30:45 +0200

    Added var for mouse

--- components/wait.vue
+++ components/wait.vue
@@ -1,3 +1,6 @@
+<script>
+const (x, y) = useMouse()
+</script>
 <template>
     <p class="group base hover:bg-second hover:shadow-glow-lg  hover:shadow-second cursor-auto hover:cursor-none hover:text-main w-fit font-mono tracking-widest text-3xl text-second transition-color duration-300">And now we wait <span class="blink border-r-[.10rem] border-second group-hover:border-main"></span></p>
 </template>
