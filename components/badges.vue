4ea40f4aeae8286e5dea6235cdfd5abc70e4afed
Flo <93609174+Xeferis@users.noreply.github.com>
2024-11-23 10:42:04 +0100

    added badges

---
+++ components/badges.vue
@@ -0,0 +1,9 @@
+<script setup>
+const data = badges();
+</script>
+
+<template>
+    <div v-for="">
+        
+    </div>
+</template>
---
+++ composables/badges.js
@@ -0,0 +1,16 @@
+export default function () {
+  const badges = [
+    {
+      name: "CCNA ITN",
+      picture:
+        "https://images.credly.com/images/70d71df5-f3dc-4380-9b9d-f22513a70417/CCNAITN__1_.png",
+      href: "https://www.credly.com/badges/b0f146ee-0588-413c-a876-8bd9d09f2116",
+    },
+    {
+      name: "Introduction to Cyber Security",
+      picture:
+        "https://images.credly.com/images/af8c6b4e-fc31-47c4-8dcb-eb7a2065dc5b/I2CS__1_.png",
+      href: "https://www.credly.com/badges/95a3df59-e231-471b-bb8a-51b66483a00d",
+    },
+  ];
+}
