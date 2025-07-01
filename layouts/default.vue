60376916039cc97f86169281be892ed28482dfe5
Florian <93609174+Xeferis@users.noreply.github.com>
2025-07-01 10:27:33 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="w-full h-full">
     <slot />
   </div>
   <img alt="GitHub Release" src="https://img.shields.io/github/v/release/Xeferis/Xeferis.github.io?style=for-the-badge&color=gray">
