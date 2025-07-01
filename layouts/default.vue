50b6a754aa46bb0319c78cddadf1b6ad6cb71350
Florian <93609174+Xeferis@users.noreply.github.com>
2025-07-01 10:33:08 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,5 +2,5 @@
   <div>
     <slot />
   </div>
-  <img class="absolut bottom-0 left-0" alt="GitHub Release" src="https://img.shields.io/github/v/release/Xeferis/Xeferis.github.io?style=for-the-badge&color=gray">
+  <!--<img class="absolut bottom-0 left-0" alt="GitHub Release" src="https://img.shields.io/github/v/release/Xeferis/Xeferis.github.io?style=for-the-badge&color=gray">-->
 </template>
