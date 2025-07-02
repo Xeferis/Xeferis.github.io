bb40593ad6169ad67464dd981d016dff078722b4
Florian <93609174+Xeferis@users.noreply.github.com>
2025-07-02 09:43:38 +0200

    show version

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,4 @@
 <template>
   <slot />
-  <!--<img class="absolut bottom-0 left-0" alt="GitHub Release" src="https://img.shields.io/github/v/release/Xeferis/Xeferis.github.io?style=for-the-badge&color=gray">-->
+  <img class="absolut bottom-0 left-0" alt="GitHub Release" src="https://img.shields.io/github/v/release/Xeferis/Xeferis.github.io?style=for-the-badge&color=gray">
 </template>
