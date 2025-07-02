390f5d007bed21f82c55e174030577fc63f2cafa
Florian <93609174+Xeferis@users.noreply.github.com>
2025-07-02 09:41:41 +0200

    removed unused div

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,4 @@
 <template>
-  <div>
-    <slot />
-  </div>
+  <slot />
   <!--<img class="absolut bottom-0 left-0" alt="GitHub Release" src="https://img.shields.io/github/v/release/Xeferis/Xeferis.github.io?style=for-the-badge&color=gray">-->
 </template>
